8d0b42c4e43167aa940300679dadfc7ffeaafe01
Gabriel Houdry--Boheme <gabriel.houdry--boheme@etudiant.univ-reims.fr>
2025-03-05 22:00:33 +0100

    Added more projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,7 +7,7 @@ const Projects = () => {
   const projectData = [
     { id: 1, name: t('thisWebsite'), description: [t('projectWebsiteDescOne'), t('projectWebsiteDescTwo')], skills: ['html5', 'css3', 'javascript', 'nodejs', 'react', 'github', 'figma'], date: '2025', img: 'img/thisWebsite.png' },
     { id: 2, name: t('cybersicknessResearch'), description: [t('cybersicknessResearchDescOne'), t('cybersicknessResearchDescTwo')], skills: ['csharp', 'unity', 'androidstudio', 'java'], date: '2024', img: 'img/cybersickness.png' },
-    { id: 3, name: 'Project Three', description: [t('projectWebsiteDescOne')], skills: ['react', 'nodejs', 'mongodb'], date: '2021', img: 'x.png' },
+    { id: 3, name: t('travelManagerProject'), description: [t('travelManagerDescOne'), t('travelManagerDescTwo')], skills: ['java', 'androidstudio', 'kotlin', 'sqlite', 'azuresqldatabase', 'figma'], date: '2024', img: 'img/travelmanager.png' },
     { id: 4, name: 'Project Four', description: [t('projectWebsiteDescOne')], skills: ['react', 'nodejs', 'mongodb'], date: '2021', img: 'x.png' },
     { id: 5, name: 'Project Five', description: [t('projectWebsiteDescOne')], skills: ['react', 'nodejs', 'mongodb'], date: '2021', img: 'x.png' }, 
 
