1f1d1f4bbd18af7a5104cad4c41def6f44c4571d
Gabriel Houdry--Boheme <gabriel.houdry--boheme@etudiant.univ-reims.fr>
2025-03-05 17:54:14 +0100

    Added project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
 
-const Project = ({ name, description }) => {
+const Project = ({ img, name, date, description, skills }) => {
+    const { t } = useTranslation();
     return (
         <div className='project-container'>
-            <p>{name}</p>
-            <p>{description}</p>
+            <div className='project-img'>
+                <img src={`${process.env.PUBLIC_URL}/${img}`} alt={name} />
+            </div>
+            <div className='project-head'>
+                <p className='projectName'>{name}</p>
+                <p className='projectDate'>{date}</p>
+                <p className='projectSkills'>{t('skills')}</p>
+                <div className='project-skills'>
+                    {skills.map(skill => (
+                        <i className={`devicon-${skill}-plain`} alt={`${name} icon`}></i>
+                    ))}
+                </div>
+            </div>
+            <div className='project-description'>
+                <p>{description}</p>
+            </div>
         </div>
     );
 };
 
 Project.propTypes = {
+    img: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
+    skills: PropTypes.array.isRequired,
 };
 
 export default Project;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,11 +3,10 @@ import useDocumentTitle from '../hooks/useDocumentTitle';
 import Project from '../components/Project';
 
 const projectData = [
-  { id: 1, name: 'Project One', description: 'Description for project one' },
-  { id: 2, name: 'Project Two', description: 'Description for project two' },
-  { id: 3, name: 'Project Three', description: 'Description for project three' },
-  { id: 4, name: 'Project Four', description: 'Description for project four' },
-  { id: 5, name: 'Project Five', description: 'Description for project five' },
+  { id: 1, name: 'THIS WEBSITE', description: 'Description for project one', skills: ['html5', 'css3', 'javascript', 'nodejs', 'react', 'github', 'figma'], date: '2025', img: 'x.png' },
+  { id: 2, name: 'Project Two', description: 'Description for project two', skills: ['react', 'typescript', 'mongodb'], date: '2021', img: 'x.png' },
+  { id: 3, name: 'Project Three', description: 'Description for project three', skills: ['react', 'nodejs', 'mongodb'], date: '2021', img: 'x.png' },
+
 ];
 
 const Projects = () => {
@@ -18,7 +17,7 @@ const Projects = () => {
       <h1>{t('projects')}</h1>
       <div className="projects">
         {projectData.map(project => (
-          <Project key={project.id} name={project.name} description={project.description} />
+          <Project key={project.id} img={project.img} name={project.name} date={project.date} skills={project.skills} description={project.description} />
         ))}
       </div>
     </div>
