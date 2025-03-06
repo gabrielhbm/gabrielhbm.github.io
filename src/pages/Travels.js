535d9134b63e3bce255933a0a07363468f8e3bf2
Gabriel Houdry--Boheme <gabriel.houdry--boheme@etudiant.univ-reims.fr>
2025-03-06 12:56:11 +0100

    Added flags instead of api call

--- src/pages/Travels.js
+++ src/pages/Travels.js
@@ -13,49 +13,31 @@ const Travels = () => {
   const [countries, setCountries] = useState([]);
 
   useEffect(() => {
-    const fetchCountries = async () => {
-      const countryData = [
-        { name: 'France', link: 'https://en.wikipedia.org/wiki/France', cities: ['Paris', 'Reims', 'Lyon', 'Brest', 'Montpellier'] },
-        { name: 'Canada', link: 'https://en.wikipedia.org/wiki/Canada', cities: ['Québec', 'Montréal', 'Saguenay'] },
-        { name: 'Germany', link: 'https://en.wikipedia.org/wiki/Germany', cities: ['Munich'] },
-        { name: 'Spain', link: 'https://en.wikipedia.org/wiki/Spain', cities: ['Madrid', 'Toledo', 'Valencia', 'Alicante'] },
-        { name: 'Italy', link: 'https://en.wikipedia.org/wiki/Italy', cities: ['Roma', 'Firenze', 'Verona', 'Pisa'] },
-        { name: 'Vatican', link: 'https://en.wikipedia.org/wiki/Vatican_City', cities: ['Vatican City'] },
-        { name: 'Cyprus', link: 'https://en.wikipedia.org/wiki/Cyprus', cities: ['Nicosia', 'Limassol', 'Paphos'] },
-        { name: 'USA', link: 'https://en.wikipedia.org/wiki/United_States', cities: ['New York City', 'Albany'] },
-        { name: 'United Kingdom', link: 'https://en.wikipedia.org/wiki/United_Kingdom', cities: ['Canterbury', 'Dover'] },
-        { name: 'Czech Republic', link: 'https://en.wikipedia.org/wiki/Czech_Republic', cities: ['Praha', 'Brno'] },
-        { name: 'Croatia', link: 'https://en.wikipedia.org/wiki/Croatia', cities: ['Split', 'Trogir'] },
-        { name: 'Bosnia', link: 'https://en.wikipedia.org/wiki/Bosnia_and_Herzegovina', cities: ['Mostar'] },
-        { name: 'Switzerland', link: 'https://en.wikipedia.org/wiki/Switzerland', cities: ['Zurich', 'Geneva', 'Basel'] },
-        { name: 'Belgium', link: 'https://en.wikipedia.org/wiki/Belgium', cities: ['Antwerp', 'Bouillon'] }
-      ];
+    const countryData = [
+      { name: 'France', link: 'https://en.wikipedia.org/wiki/France', cities: ['Paris', 'Reims', 'Lyon', 'Brest', 'Montpellier'], flag: `${process.env.PUBLIC_URL}/flags/fr.svg` },
+      { name: 'Canada', link: 'https://en.wikipedia.org/wiki/Canada', cities: ['Québec', 'Montréal', 'Saguenay'], flag: `${process.env.PUBLIC_URL}/flags/ca.svg` },
+      { name: 'Germany', link: 'https://en.wikipedia.org/wiki/Germany', cities: ['Munich'], flag: `${process.env.PUBLIC_URL}/flags/de.svg` },
+      { name: 'Spain', link: 'https://en.wikipedia.org/wiki/Spain', cities: ['Madrid', 'Toledo', 'Valencia', 'Alicante'], flag: `${process.env.PUBLIC_URL}/flags/es.svg` },
+      { name: 'Italy', link: 'https://en.wikipedia.org/wiki/Italy', cities: ['Roma', 'Firenze', 'Verona', 'Pisa'], flag: `${process.env.PUBLIC_URL}/flags/it.svg` },
+      { name: 'Vatican', link: 'https://en.wikipedia.org/wiki/Vatican_City', cities: ['Vatican City'], flag: `${process.env.PUBLIC_URL}/flags/va.svg` },
+      { name: 'Cyprus', link: 'https://en.wikipedia.org/wiki/Cyprus', cities: ['Nicosia', 'Limassol', 'Paphos'], flag: `${process.env.PUBLIC_URL}/flags/cy.svg` },
+      { name: 'USA', link: 'https://en.wikipedia.org/wiki/United_States', cities: ['New York City', 'Albany'], flag: `${process.env.PUBLIC_URL}/flags/us.svg` },
+      { name: 'United Kingdom', link: 'https://en.wikipedia.org/wiki/United_Kingdom', cities: ['Canterbury', 'Dover'], flag: `${process.env.PUBLIC_URL}/flags/gb.svg` },
+      { name: 'Czech Republic', link: 'https://en.wikipedia.org/wiki/Czech_Republic', cities: ['Praha', 'Brno'], flag: `${process.env.PUBLIC_URL}/flags/cz.svg` },
+      { name: 'Croatia', link: 'https://en.wikipedia.org/wiki/Croatia', cities: ['Split', 'Trogir'], flag: `${process.env.PUBLIC_URL}/flags/hr.svg` },
+      { name: 'Bosnia', link: 'https://en.wikipedia.org/wiki/Bosnia_and_Herzegovina', cities: ['Mostar'], flag: `${process.env.PUBLIC_URL}/flags/ba.svg` },
+      { name: 'Switzerland', link: 'https://en.wikipedia.org/wiki/Switzerland', cities: ['Zurich', 'Geneva', 'Basel'], flag: `${process.env.PUBLIC_URL}/flags/ch.svg` },
+      { name: 'Belgium', link: 'https://en.wikipedia.org/wiki/Belgium', cities: ['Antwerp', 'Bouillon'], flag: `${process.env.PUBLIC_URL}/flags/be.svg` }
+    ];
 
-      const fetchFlag = async (countryName) => {
-        const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
-        const data = await response.json();
-        return data[0].flags.png;
-      };
-
-      const countriesWithFlags = await Promise.all(
-        countryData.map(async (country) => {
-          const flag = await fetchFlag(country.name);
-          return { ...country, flag };
-        })
-      );
-
-      setCountries(countriesWithFlags);
-    };
-
-    fetchCountries();
+    setCountries(countryData);
   }, []);
 
   return (
     <div>
       <Map />
       <div className="travels-container">
-        
-      <h1 className='travelsTitle'>{t('travels')}</h1>
+        <h1 className='travelsTitle'>{t('travels')}</h1>
         <p className="travelsDesc">{t('travelsDescription')}</p>
         <div className="countries-wrapper">
           {countries.map((country, index) => (
